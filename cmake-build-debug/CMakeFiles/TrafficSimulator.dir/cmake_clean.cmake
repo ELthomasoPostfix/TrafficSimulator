5fb1b64cb681c3b075ff95c764812e9a4851f579
Thomas Gueutal <59799828+ELthomasoPostfix@users.noreply.github.com>
2020-05-10 10:44:44 +0200

    Add files via upload
    the base files when starting a new project

---
+++ cmake-build-debug/CMakeFiles/TrafficSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrafficSimulator.dir/main.cpp.o"
+  "TrafficSimulator"
+  "TrafficSimulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrafficSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
